ab8928371e2dd43d32c6b444f20a7755d5ef19e0
NikUvolen <mixa2003777@gmail.com>
2026-07-28 12:43:28 +0800

    Implement ReviewParsingService with provider-specific parsers and error handling

---
+++ review_parser/common_parser/parsing/providers/__init__.py
@@ -0,0 +1,10 @@
+from .base import BaseReviewParser
+from .twogis import TwoGisParser
+from .vlru import VlRuParser
+
+
+__all__ = [
+    'BaseReviewParser',
+    'TwoGisParser',
+    'VlRuParser',
+]

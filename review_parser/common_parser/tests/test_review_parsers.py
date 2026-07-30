d59c7c10c8f5f3578a75fa9b315ebe804b69dad8
NikUvolen <mixa2003777@gmail.com>
2026-07-31 01:40:24 +0800

    Add tests for YandexParser functionality and review parsing

--- review_parser/common_parser/tests/test_review_parsers.py
+++ review_parser/common_parser/tests/test_review_parsers.py
@@ -2,3 +2,4 @@ from common_parser.tests.test_twogis_client import *  # noqa: F403
 from common_parser.tests.test_twogis_parser import *  # noqa: F403
 from common_parser.tests.test_vlru_client import *  # noqa: F403
 from common_parser.tests.test_vlru_parser import *  # noqa: F403
+from common_parser.tests.test_yandex_parser import *  # noqa: F403
